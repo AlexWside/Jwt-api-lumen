9d95f30a77c3d92ff8fa2abc54aa132b6ac060c8
Alexsandro Fonseca <alexsandro.akila@outlook.com>
2021-12-28 20:41:06 -0400

    first

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+
+//import auth facades
+use Illuminate\Support\Facades\Auth;
+
+class Controller extends BaseController
+{
+    //
+
+
+
+
+    //Add this method to the Controller class
+    protected function respondWithToken($token)
+      {
+          return response()->json([
+              'token' => $token,
+              'token_type' => 'bearer',
+              'expires_in' => Auth::factory()->getTTL() * 60
+          ], 200);
+      }
+
+}
+
+
