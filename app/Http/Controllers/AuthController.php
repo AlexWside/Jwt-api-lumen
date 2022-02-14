3a5bc0030c6d0afe2191e5dc2a03cac68d8dbfb9
alexwside <alexsandro.akila@outlook.com>
2022-02-14 09:51:31 -0400

    fix return

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -62,7 +62,7 @@ class AuthController extends Controller
         $credentials = $request->only(['email', 'password']);
 
         if (! $token = Auth::attempt($credentials)) {
-            return response()->json(['message' => 'Unauthorized'], 401);
+            return response()->json(['message' => 'Email ou senha invalidos'], 401);
         }
 
 
